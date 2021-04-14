72d7bbd6dad3f9cb8738ce108392dba4c358c673
jarvis394 <tarnatovski@gmail.com>
2021-04-14 18:43:15 +0300

    Add new service worker

--- src/pages/User/Contacts.tsx
+++ src/pages/User/Contacts.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Typography, Grid, Link } from '@material-ui/core'
-import parse, { HTMLReactParserOptions } from 'html-react-parser'
 import { ComponentWithUserParams } from './index'
 import { makeStyles } from '@material-ui/core/styles'
 import { useSelector } from 'src/hooks'
@@ -29,35 +28,26 @@ export const Contacts = ({
   classes: additionalClasses,
 }: ComponentWithUserParams) => {
   const classes = useStyles()
-  const user = useSelector((store) => store.profile.profile.card.data)
-  const options: HTMLReactParserOptions = {
-    replace: ({ name, children, attribs }): void | React.ReactElement => {
-      if (name === 'a') {
-        return (
-          <Link
-            className={classes.link}
-            href={attribs.href}
-            rel="nofollow noopener"
-            target="_blank"
-          >
-            {children[0].data}
-          </Link>
-        )
-      }
-    },
-  }
-  return null
-  // return user.contacts.length !== 0 ? (
-  //   <div className={additionalClasses}>
-  //     <Typography className={classes.blockTitle}>Контакты</Typography>
-  //     <Grid container>
-  //       {user.contacts.map((e, i) => (
-  //         <Grid key={i} item className={classes.contactsItem}>
-  //           <Typography>{e.title}</Typography>
-  //           {parse(e.link, options)}
-  //         </Grid>
-  //       ))}
-  //     </Grid>
-  //   </div>
-  // ) : null
+  const whois = useSelector((store) => store.profile.profile.whois.data)
+
+  return whois.contacts.length !== 0 ? (
+    <div className={additionalClasses}>
+      <Typography className={classes.blockTitle}>Контакты</Typography>
+      <Grid container>
+        {whois.contacts.map((e, i) => (
+          <Grid key={i} item className={classes.contactsItem}>
+            <Typography>{e.title}</Typography>
+            <Link
+              className={classes.link}
+              href={e.url}
+              rel="nofollow noopener"
+              target="_blank"
+            >
+              {e.url}
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  ) : null
 }
